98b6fa99ce4bbba7ab037591673dd6cbdffe6018
canorsce <92429263+canorsce@users.noreply.github.com>
2021-11-05 09:21:48 +0100

    Add files via upload

---
+++ JS/Portofolio.js
@@ -0,0 +1,11 @@
+function openTab(th){
+    window.open(th.name,'PDF');
+}
+
+const Aboutme = document.getElementById("About-me");
+
+window.addEventListener("scroll", function(e_){
+
+    let offset = window.pageYOffset;
+    Aboutme.style.backgroundPositionY = offset * 0.7 + "px"
+});
